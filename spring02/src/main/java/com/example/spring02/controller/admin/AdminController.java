dd62bc28d3827afb5adf87e15a126a6f1bdf4a79
janghyeonwoo <blue672@naver.com>
2021-10-04 16:44:54 +0900

    로그4

--- spring02/src/main/java/com/example/spring02/controller/admin/AdminController.java
+++ spring02/src/main/java/com/example/spring02/controller/admin/AdminController.java
@@ -31,7 +31,7 @@ public class AdminController {
 			session.setAttribute("name", name);
 			mav.setViewName("admin/admin");
 			mav.addObject("message", "success");
-			/*로그2*/
+			/*로그4*/
 		}else {
 			mav.setViewName("admin/login");
 			mav.addObject("message","error");
